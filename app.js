5ab748572328e9ea8c5ed5220051fe03a9f584f3
Marcell Spies <marcells@gmx.de>
2015-02-15 15:23:51 +0100

    Initial checkin.

---
+++ app.js
@@ -0,0 +1,26 @@
+var config = require('./config.json');
+var express = require('express');
+var exec = require('child_process').exec;
+var app = express();
+
+app.get('/:hook/:token', function(req, res) {
+  var hook = config[req.params.hook];
+
+  if (!hook || hook.token !== req.params.token) {
+    res.sendStatus(404);
+    return;
+  }
+
+  exec('~/script.sh', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+
+    res.sendStatus(200);
+  });
+});
+
+app.listen(3000);
